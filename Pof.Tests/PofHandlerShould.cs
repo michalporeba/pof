47cca4fe4fe867771577994a60ce763747958ea8
Michał Poręba <michalporeba@gmail.com>
2020-04-27 23:37:20 +0100

    handler generates messages from simply properties

--- Pof.Tests/PofHandlerShould.cs
+++ Pof.Tests/PofHandlerShould.cs
@@ -1,35 +1,62 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Pof.Tests
 {
     public class PofEntityShould
     {
+        private PofHandler<TestEntity> _handler;
+
         [SetUp]
         public void Setup()
         {
+            _handler = new PofHandler<TestEntity>();
         }
 
         [Test]
         public void expose_instantiated_entity()
         {
-            var handler = new PofHandler<TestEntity>();
-            Assert.That(handler.Entity, Is.Not.Null);
+            Assert.That(_handler.Entity, Is.Not.Null);
         }
 
         [TestCase("abc")]
         [TestCase("cba")]
-        public void update_property_from_message(string value)
+        public void update_string_property_from_message(string value)
         {
-            var handler = new PofHandler<TestEntity>();
-            handler.Handle(new Message(nameof(TestEntity.StringProperty), value));
-            Assert.That(handler.Entity.StringProperty, Is.EqualTo(value));
+            _handler.Handle(new Message(nameof(TestEntity.StringProperty), value));
+            Assert.That(_handler.Entity.StringProperty, Is.EqualTo(value));
+        }
+
+        [TestCase(11)]
+        [TestCase(145)]
+        public void update_string_property_from_message(int value)
+        {
+            _handler.Handle(new Message(nameof(TestEntity.IntegerProperty), value));
+            Assert.That(_handler.Entity.IntegerProperty, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void have_no_new_messages_when_no_changes_have_been_made_to_the_entity()
+        {
+            Assert.That(_handler.GetNewMessages(), Is.Empty);
+        }
+
+        [TestCase("StringProperty", "a")]
+        [TestCase("StringProperty", "b")]
+        public void have_message_for_the_changed_properties(string propertyName, object value)
+        {
+            _handler.Entity.StringProperty = value.ToString();
+            var message = _handler.GetNewMessages().FirstOrDefault();
+            Assert.That(message.PropertyName, Is.EqualTo(propertyName), "Wrong property name");
+            Assert.That(message.Value, Is.EqualTo(value), "Value is incorrect");
         }
 
         public class TestEntity : IPofEntity
         {
-            public Guid Id { get; } = Guid.NewGuid();
+            public Guid Id { get; } = Guid.Empty;
             public string StringProperty { get; set; }
+            public int IntegerProperty { get; set; }
         }
     }
 }
--- Pof/PofHandler.cs
+++ Pof/PofHandler.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Pof
 {
     public class PofHandler<TEntity>
         where TEntity : IPofEntity, new()
     {
+        private readonly TEntity _original = new TEntity();
         public TEntity Entity { get; } = new TEntity();
         public void Handle(Message message)
         {
             var property = Entity.GetType().GetProperty(message.PropertyName);
             property.SetValue(Entity, message.Value);
+        }
+
+        public IEnumerable<Message> GetNewMessages()
+        {
+            foreach(var property in Entity.GetType().GetProperties())
+            {
+                var originalValue = property.GetValue(_original);
+                var newValue = property.GetValue(Entity);
+
+                if ((originalValue == null && newValue != null)
+                    || (originalValue != null && !originalValue.Equals(newValue))
+                )
+                {
+                    yield return new Message(property.Name, newValue);
+                }
+            }
         }
     }
 }
