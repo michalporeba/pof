db3d5a1e0252f756cbe6e2b36db1f370a6ebe60a
Michał Poręba <michalporeba@gmail.com>
2020-05-08 17:40:24 +0100

    improvements to the in memory message store

--- Pof.Tests/MessagePumpShould.cs
+++ Pof.Tests/MessagePumpShould.cs
@@ -7,26 +7,28 @@ namespace Pof.Tests
 {
     public class MessagePumpShould
     {
-        private Mock<IMessageStore> _messageStore = new Mock<IMessageStore>();
-        private string _testTopic = string.Empty;
+        private readonly IMessageStore _messageStore = new InMemoryMessageStore();
 
         [SetUp]
         public void SetUp()
         {
-            _messageStore = new Mock<IMessageStore>();
-            _testTopic = Guid.NewGuid().ToString().Substring(0, 8);
+            
         }
         
         [Test, Ignore("WIP")]
         public void store_any_incoming_message_in_a_local_storage()
         {
             var pump = CreateTestPump();
-            pump.Push(_testTopic, new Message());
+            var topic = Guid.NewGuid().ToString();
+            var message = new Message();
+            pump.Push(topic, message);
+            
+            //_messageStore.
         }
 
         private IMessagePump CreateTestPump()
         {
-            return new MessagePump(_messageStore.Object);
+            return new MessagePump(_messageStore);
         }
     }
 }
--- Pof/Data/InMemoryMessageStore.cs
+++ Pof/Data/InMemoryMessageStore.cs
@@ -6,8 +6,14 @@ namespace Pof.Data
 {
     public class InMemoryMessageStore : IMessageStore
     {
-        private readonly Dictionary<string, IList<Message>> _topics = new Dictionary<string, IList<Message>>();  
+        private readonly Dictionary<string, IList<Message>> _topics = new Dictionary<string, IList<Message>>();
 
+        public bool Contains(string topic, Message message)
+        {
+            return _topics.ContainsKey(topic) 
+                   && _topics[topic].Any(m => m.Hash == message.Hash);
+        }
+        
         public void Save(string topic, Message message)
         {
             if (!_topics.ContainsKey(topic))
